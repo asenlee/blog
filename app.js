c18d358156a2aa365f40dd205638301a678b42f3
Asen Lee <woochang.lee0405@gmail.com>
2022-11-14 21:33:17 -0800

    update

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash");
+const { lowerCase } = require("lodash");
+
 let posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -21,6 +24,19 @@ app.get("/", function(req, res) {
   res.render("home", {
     startingContent: homeStartingContent, 
     posts: posts});
+});
+
+app.get("/posts/:postID", function(req, res) {
+  const requestedTitle = lodash.lowerCase(req.params.postID);
+
+  let postPos = posts.map(function(x) {return lodash.lowerCase(x.title);}).indexOf(requestedTitle);
+  let requestedPost = posts[postPos];
+
+  res.render("post", {
+    postTitle: requestedPost.title,
+    postContent: requestedPost.content
+  });
+
 });
 
 app.get("/about", function(req, res) {
